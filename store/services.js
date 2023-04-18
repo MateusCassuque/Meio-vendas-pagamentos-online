0a26030da2644ec88ece2fe8c050e0befc2c218b
Mateus Cassuque <100739934+MateusCassuque@users.noreply.github.com>
2023-04-18 01:21:03 +0100

    preparando para o ambiente de produção

--- store/services.js
+++ store/services.js
@@ -7,7 +7,8 @@ const setBearerToken = token => {
 
 const api = axios.create({
     // baseURL: 'http://192.168.1.100:3000/'
-    baseURL: 'http://127.0.0.1:3000/'
+    // baseURL: 'http://127.0.0.1:3000/'
+    baseURL: 'https://mvpo-back-end.onrender.com/'
 })
 
 
